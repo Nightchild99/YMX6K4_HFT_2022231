dc23b3e9970b9db4759962f7c08bee8edf969b13
Nightchild <dmnks.peter@gmail.com>
2022-10-18 10:26:32 +0200

    Add project files.

---
+++ YMX6K4_HFT_2022231.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YMX6K4_HFT_2022231.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
