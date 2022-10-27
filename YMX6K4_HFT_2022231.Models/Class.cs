271441e5b140d857bf9a15cce3c1556553d08648
Nightchild <dmnks.peter@gmail.com>
2022-10-27 11:47:53 +0200

    class class extended

--- YMX6K4_HFT_2022231.Models/Class.cs
+++ YMX6K4_HFT_2022231.Models/Class.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace YMX6K4_HFT_2022231.Models
 {
-    public enum Type {tank, melee, ranged, caster, support, healer}
+    public enum Type {tank, melee, ranged, caster, support, healer, various}
+
+    [Table("Classes")]
     public class Class
     {
         [Key]
@@ -22,5 +24,52 @@ namespace YMX6K4_HFT_2022231.Models
 
         [Required]
         public bool Allowed { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Player> Players { get; set; }
+
+        public Class()
+        {
+            Players = new HashSet<Player>();
+        }
+
+        public Class(string line)
+        {
+            string[] datas = line.Split('#');
+            ID = int.Parse(datas[0]);
+            Name = datas[1];
+            switch (datas[2])
+            {
+                case "tank":
+                    Type = Type.tank;
+                    break;
+                case "melee":
+                    Type = Type.melee;
+                    break;
+                case "ranged":
+                    Type = Type.ranged;
+                    break;
+                case "caster":
+                    Type = Type.caster;
+                    break;
+                case "support":
+                    Type = Type.support;
+                    break;
+                case "healer":
+                    Type = Type.healer;
+                    break;
+                case "various":
+                    Type = Type.various;
+                    break;
+            }
+            if (datas[3] == "0")
+            {
+                Allowed = false;
+            }
+            else
+            {
+                Allowed = true;
+            }
+        }
     }
 }
