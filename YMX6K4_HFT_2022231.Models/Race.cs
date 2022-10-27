415274b023d7b36dea40315be0c62fcf4da99970
Nightchild <dmnks.peter@gmail.com>
2022-10-27 11:04:47 +0200

    race class created

---
+++ YMX6K4_HFT_2022231.Models/Race.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMX6K4_HFT_2022231.Models
+{
+    public class Race
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [StringLength(6)]
+        public string Source { get; set; }
+
+        [Required]
+        public bool Allowed { get; set; }
+    }
+}
