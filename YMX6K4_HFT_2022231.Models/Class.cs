3b28d3088f9b97f4d213cff2fb74be0171765828
Nightchild <dmnks.peter@gmail.com>
2022-10-27 11:08:35 +0200

    class class created

---
+++ YMX6K4_HFT_2022231.Models/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMX6K4_HFT_2022231.Models
+{
+    public enum Type {tank, melee, ranged, caster, support, healer}
+    public class Class
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [StringLength(15)]
+        public string Name { get; set; }
+
+        public Type Type { get; set; }
+
+        [Required]
+        public bool Allowed { get; set; }
+    }
+}
