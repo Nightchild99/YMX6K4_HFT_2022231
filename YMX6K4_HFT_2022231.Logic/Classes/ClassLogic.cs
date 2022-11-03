4af04b0445a70d0623e74383a4971331de6dd209
Nightchild <dmnks.peter@gmail.com>
2022-11-03 14:57:18 +0100

    class logic added

---
+++ YMX6K4_HFT_2022231.Logic/Classes/ClassLogic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMX6K4_HFT_2022231.Logic.Interfaces;
+using YMX6K4_HFT_2022231.Models.Models;
+using YMX6K4_HFT_2022231.Repository.Interface;
+
+namespace YMX6K4_HFT_2022231.Logic.Classes
+{
+    public class ClassLogic : IClassLogic
+    {
+        IRepository<Class> repo;
+
+        public ClassLogic(IRepository<Class> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Class item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Class Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Class> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Class item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
---
+++ YMX6K4_HFT_2022231.Logic/Interfaces/IClassLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMX6K4_HFT_2022231.Models.Models;
+
+namespace YMX6K4_HFT_2022231.Logic.Interfaces
+{
+    public interface IClassLogic
+    {
+        void Create(Class item);
+        Class Read(int id);
+        IQueryable<Class> ReadAll();
+        void Update(Class item);
+        void Delete(int id);
+    }
+}
