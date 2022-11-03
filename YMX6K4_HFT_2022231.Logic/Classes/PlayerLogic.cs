d0e536dbf453ded4b03d543086c984719db55787
Nightchild <dmnks.peter@gmail.com>
2022-11-03 14:59:47 +0100

    player logic created

---
+++ YMX6K4_HFT_2022231.Logic/Classes/PlayerLogic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMX6K4_HFT_2022231.Logic.Interfaces;
+using YMX6K4_HFT_2022231.Models.Models;
+using YMX6K4_HFT_2022231.Repository.Interface;
+
+namespace YMX6K4_HFT_2022231.Logic.Classes
+{
+    public class PlayerLogic : IPlayerLogic
+    {
+        IRepository<Player> repo;
+
+        public PlayerLogic(IRepository<Player> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Player item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Player Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Player> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Player item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
---
+++ YMX6K4_HFT_2022231.Logic/Interfaces/IPlayerLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMX6K4_HFT_2022231.Models.Models;
+
+namespace YMX6K4_HFT_2022231.Logic.Interfaces
+{
+    public interface IPlayerLogic
+    {
+        void Create(Player item);
+        Player Read(int id);
+        IQueryable<Player> ReadAll();
+        void Update(Player item);
+        void Delete(int id);
+    }
+}
