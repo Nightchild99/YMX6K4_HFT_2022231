bff23b0cb35cef708b23cd2ecec6af11208eb682
Nightchild <dmnks.peter@gmail.com>
2022-11-03 14:53:37 +0100

    race logic created

---
+++ YMX6K4_HFT_2022231.Logic/Classes/RaceLogic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMX6K4_HFT_2022231.Logic.Interfaces;
+using YMX6K4_HFT_2022231.Models.Models;
+using YMX6K4_HFT_2022231.Repository.Interface;
+
+namespace YMX6K4_HFT_2022231.Logic.Classes
+{
+    public class RaceLogic : IRaceLogic
+    {
+        IRepository<Race> repo;
+
+        public RaceLogic(IRepository<Race> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Race item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Race Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Race> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Race item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
---
+++ YMX6K4_HFT_2022231.Logic/Interfaces/IRaceLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMX6K4_HFT_2022231.Models.Models;
+
+namespace YMX6K4_HFT_2022231.Logic.Interfaces
+{
+    public interface IRaceLogic
+    {
+        void Create(Race item);
+        Race Read(int id);
+        IQueryable<Race> ReadAll();
+        void Update(Race item);
+        void Delete(int id);
+    }
+}
