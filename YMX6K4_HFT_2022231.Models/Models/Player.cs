81cbda110b717216337d2fa5a5548ce91307a792
Nightchild <dmnks.peter@gmail.com>
2022-11-22 12:55:11 +0100

    test1 + more navigation properties

--- YMX6K4_HFT_2022231.Models/Models/Player.cs
+++ YMX6K4_HFT_2022231.Models/Models/Player.cs
@@ -55,5 +55,23 @@ namespace YMX6K4_HFT_2022231.Models.Models
             ClassID = int.Parse(datas[4]);
             Level = rnd.Next(4, 7);
         }
+
+        public override bool Equals(object obj)
+        {
+            Player b = obj as Player;
+
+            if (b == null)
+            {
+                return false;
+            }
+            else
+            {
+                return this.ID == b.ID
+                    && this.Name == this.Name
+                    && this.CharacterName == b.CharacterName
+                    && this.RaceID == b.RaceID
+                    && this.ClassID == b.ClassID;
+            }
+        }
     }
 }
--- YMX6K4_HFT_2022231.Test/PlayerLogicTest.cs
+++ YMX6K4_HFT_2022231.Test/PlayerLogicTest.cs
@@ -8,6 +8,7 @@ using YMX6K4_HFT_2022231.Repository.Interface;
 
 namespace YMX6K4_HFT_2022231.Test
 {
+    [TestFixture]
     public class PlayerLogicTest
     {
         PlayerLogic logic;
@@ -23,15 +24,35 @@ namespace YMX6K4_HFT_2022231.Test
             var dora = new Player("5#Dóra#Echo#5#13");
 
             var ratfolk = new Race("31#Ratfolk#dndb#1");
+            ratfolk.Players = new HashSet<Player>();
             var dragonborn = new Race("6#Dragonborn#phb#1");
+            dragonborn.Players = new HashSet<Player>();
             var elf = new Race("8#Elf#phb#1");
+            elf.Players = new HashSet<Player>();
             var fairy = new Race("9#Fairy#wbtw#1");
+            fairy.Players = new HashSet<Player>();
             var changeling = new Race("5#Changeling#erlw#0");
+            changeling.Players = new HashSet<Player>();
 
             var barbarian = new Class("2#Barbarian#tank#1");
+            barbarian.Players = new HashSet<Player>();
             var cleric = new Class("5#Cleric#healer#1");
+            cleric.Players = new HashSet<Player>();
             var ranger = new Class("12#Ranger#ranged#1");
+            ranger.Players = new HashSet<Player>();
             var rogue = new Class("13#Rogue#melee#1");
+            rogue.Players = new HashSet<Player>();
+
+            bogi.Race = ratfolk;
+            bogi.Class = ranger;
+            gyula.Race = dragonborn;
+            gyula.Class = barbarian;
+            anita.Race = elf;
+            anita.Class = cleric;
+            tamara.Race = fairy;
+            tamara.Class = rogue;
+            dora.Race = changeling;
+            dora.Class = rogue;
 
             ratfolk.Players.Add(bogi);
             dragonborn.Players.Add(gyula);
@@ -52,6 +73,16 @@ namespace YMX6K4_HFT_2022231.Test
             mockPlayerRepository = new Mock<IRepository<Player>>();
             mockPlayerRepository.Setup(m => m.ReadAll()).Returns(players);
             logic = new PlayerLogic(mockPlayerRepository.Object);
+        }
+
+        [Test]
+        public void PlayerReadTest()
+        {
+            var expected = new Player("1#Bogi#Mr. Pöpi#31#12");
+            mockPlayerRepository.Setup(p => p.Read(1)).Returns(expected);
+            var actual = logic.Read(1);
+
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
